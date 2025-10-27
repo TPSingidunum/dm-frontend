55f6bdd363a0c2e8494b2ce6d0909d95e31673d8
TPSingidunum <tpetrovic@singidunum.ac.rs>
2025-10-27 13:41:49 +0100

    Configured NuxtUI componnets, addes css, and made most of the basic pages we are going to use

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,39 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const route = useRoute()
+
+const items = computed<NavigationMenuItem[]>(() => [
+{
+    label: 'Home',
+    to: '/',
+    active: route.path == "/"
+  },
+  {
+    label: 'Post',
+    to: '/post',
+    active: route.path.startsWith('/post')
+  },
+  {
+    label: 'Category',
+    to: '/category',
+    active: route.path.startsWith('/category')
+  },
+])
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <UHeader>
+        <UNavigationMenu :items="items" />
+    </UHeader>
+
+    <UMain>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </UMain>
+
+    <UFooter />
+  </UApp>
 </template>
