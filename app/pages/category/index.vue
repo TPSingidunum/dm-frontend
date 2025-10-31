c79535a307c4add5b57d3c18cf4e482fec00210f
TPSingidunum <tpetrovic@singidunum.ac.rs>
2025-10-31 13:33:41 +0100

    Added sample pages for category and for product

--- app/pages/category/index.vue
+++ app/pages/category/index.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <h1>category</h1>
+    <h1 class="text-center pt-10 text-2xl font-bold">Category</h1>
+    <div class="p-10 grid grid-cols-3 gap-2">
+      <UBlogPost
+        v-for="category in categories"
+        class="w-[400px]"
+        :title="category.name"
+        :to="`/category/${category.slug}`"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 
+const categories = await $fetch("/api/category");
+
 </script>
-
-<style>
-
-</style>
--- app/pages/product/index.vue
+++ app/pages/product/index.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-    <h1>Hello</h1>
+    <h1 class="text-center pt-10 text-2xl font-bold">Product</h1>
+    <div class="p-10 grid grid-cols-3 gap-2">
+      <UBlogPost
+        v-for="product in products"
+        class="w-[400px]"
+        :title="product.name"
+        :description="product.description"
+        :to="`/product/${product.slug}`"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 
+const products = await $fetch("/api/product");
+
 </script>
-
-<style>
-
-</style>
